aa219620c34250a0fbaf335e5ea98916fa92d607
perre78 <per.melinder@gmail.com>
2026-01-22 23:10:40 +0100

    Update language-specific HTML files and sitemap URLs to reflect new domain structure. Prepeared for move to loopia.se and the domain ekonomirum.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-const isProd = process.env.NODE_ENV === "production";
-
 export default defineConfig({
-  site: "https://perre78.github.io",      // VIKTIGT: domän utan repo-namn
-  base: isProd ? "/ekonomirum/" : "/",    // repo-namnet i prod
+  site: "https://ekonomirum.se",
+  base: "/",
   outDir: "./docs",
-
-  integrations: [
-    sitemap(),
-  ],
+  integrations: [sitemap()],
 });
+
