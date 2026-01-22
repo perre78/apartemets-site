8c2d858978bdf3cc8806fd3a754d262d29676067
perre78 <per.melinder@gmail.com>
2026-01-23 00:02:40 +0100

    Update URLs in HTML files and sitemaps to use the correct domain format

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://ekonomirum.se",
+  site: "https://www.ekonomirum.se",
   base: "/",
   outDir: "./docs",
   integrations: [sitemap()],
